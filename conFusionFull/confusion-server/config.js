0c38170792e8828b4ecc9d340a417d6ce5abb0bb
unknown <mahana.deepak20@gmail.com>
2017-12-18 19:14:40 +0530

    AngularCoursera

---
+++ conFusionFull/confusion-server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://Dcoder:Dmahana123@ds161136.mlab.com:61136/restaurant',
+    'facebook': {
+        clientId: 'Your App ID',
+        clientSecret: 'Your App Secret'
+    }
+}
+
